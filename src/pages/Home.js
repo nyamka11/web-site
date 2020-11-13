0cb9200d2fff8c0c4be549fb51a4acf20d45a462
h-mochizuki <Nyamka>
2020-11-13 14:04:08 +0900

    hamgiin suuliin dajgui huwilbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import  React, { useState  } from 'react';
+import React, { useState, useEffect } from "react";
 import  NavbarComponent  from '../components/common/Navbar.js';
 import  Footer  from '../components/common/Footer.js';
 import leaflet from 'leaflet';
@@ -6,6 +6,7 @@ import { MapContainer, TileLayer, Marker, Popup, Polyline, Circle, CircleMarker,
 import pointer from '../assets/pointer.svg';
 import bluePointer from '../assets/bluePointer.svg';
 import * as parkData from "../data/skateboard-parks.json";
+import Chart from "chart.js";
 
 
 const Home = () => {
@@ -24,6 +25,32 @@ const Home = () => {
         [34.690039, 137.747250]
       ]
     ]
+
+    useEffect(() => {
+        const ctx = document.getElementById("myChart");
+        new Chart(ctx, {
+            type: "pie",
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [
+                {
+                    label: "# of Votes",
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                    "Red",
+                    "Blue",
+                    "Yellow",
+                    "Green",
+                    "Purple",
+                    "Orange"
+                    ],
+                    borderColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                    borderWidth: 1
+                }
+                ]
+            }
+        });
+    });
 
     return (
         <div>
@@ -82,7 +109,7 @@ const Home = () => {
                     
             </MapContainer>
             <div>
-                asdf
+                <canvas id="myChart" width="400" height="400" />
             </div>
             <Footer />
         </div>
